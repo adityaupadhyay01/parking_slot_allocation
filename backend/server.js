a0bf8977367fddc15e0db118403611e12420b61d
adityaupadhyay01 <upadhyayaditya432@gmail.com>
2026-03-23 23:09:31 +0530

    added Nodejs

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,39 @@
 const express = require("express");
 const { exec } = require("child_process");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// absolute path to exe
+const exePath = path.join(__dirname, "../c_engine/parking.exe");
+
+// PARK
 app.post("/park", (req, res) => {
     const vehicle = req.body.vehicle;
 
-    exec(`../c_engine/parking.exe park ${vehicle}`, (err, stdout) => {
-        if (err) return res.send("Error");
+    exec(`"${exePath}" park ${vehicle}`, (err, stdout, stderr) => {
+        if (err) {
+            console.log("ERROR:", err);
+            console.log("STDERR:", stderr);
+            return res.send("Error running C program");
+        }
         res.send(stdout);
     });
 });
 
+// EXIT
 app.post("/exit", (req, res) => {
     const vehicle = req.body.vehicle;
 
-    exec(`../c_engine/parking.exe exit ${vehicle}`, (err, stdout) => {
-        if (err) return res.send("Error");
+    exec(`"${exePath}" exit ${vehicle}`, (err, stdout, stderr) => {
+        if (err) {
+            console.log("ERROR:", err);
+            console.log("STDERR:", stderr);
+            return res.send("Error running C program");
+        }
         res.send(stdout);
     });
 });
